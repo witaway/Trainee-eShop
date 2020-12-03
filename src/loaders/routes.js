18665209a3598094477d890155f2757c67731164
Yegor Levonenko <witaway33@gmail.com>
2020-12-03 17:53:53 +0300

    Updated routes

--- src/loaders/routes.js
+++ src/loaders/routes.js
@@ -1,3 +1,5 @@
 module.exports = function(server) {
-    server.use('/products', require('../routes/product'))
+    server.use('/products', require('../routes/products'))
+    server.use('/auth',     require('../routes/auth'))
+    server.use('/users',    require('../routes/users'))
 }
