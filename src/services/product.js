5c16094c4d90a4b5de0521a60f10036dcc868d34
Yegor Levonenko <witaway33@gmail.com>
2021-01-19 22:20:37 +0300

    Small fix (service uses repository !NOT controller hahaha)

--- src/services/product.js
+++ src/services/product.js
@@ -1,29 +1,29 @@
-const ProductController = require('../controllers/product')
+const ProductRepository = require('../repositories/product')
 
 class ProductService {
 
     static async createProduct(body) {
-        const createdProduct = ProductController.createProduct(body)
+        const createdProduct = ProductRepository.createProduct(body)
         return createdProduct
     }
 
     static async getProduct(body) {
-        const product = ProductController.getProduct(body) 
+        const product = ProductRepository.getProduct(body) 
         return product
     }
 
     static async getAllProducts(body) {
-        const products = ProductController.getAllProducts(body)
+        const products = ProductRepository.getAllProducts(body)
         return products
     }
 
     static async editProduct(body) {
-        const newProduct = ProductController.editProduct(body)
+        const newProduct = ProductRepository.editProduct(body)
         return newProduct
     }
 
     static async deleteProduct(body) {
-        const deletedProduct = ProductController.deleteProduct(body)
+        const deletedProduct = ProductRepository.deleteProduct(body)
         return deletedProduct
     }
 
