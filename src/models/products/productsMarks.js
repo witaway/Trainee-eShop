5e3f4cc683daddfd0148175abc01e31b2aceebce
Yegor Levonenko <witaway33@gmail.com>
2021-01-20 19:35:08 +0300

    Connection between tables were incorrect and i couldn't get marks for products.

--- src/models/products/productsMarks.js
+++ src/models/products/productsMarks.js
@@ -21,8 +21,12 @@ const ProductsMarks = sequelize.define('productsMarks', {
 })
 
 ProductsMarks.associate = (models) => {
-    models.Product.belongsToMany(models.User, { through: models.ProductsMarks })
-    models.User.belongsToMany(models.Product, { through: models.ProductsMarks })
+    models.Product.hasMany(models.ProductsMarks);
+    models.User.hasMany(models.ProductsMarks);
+    //models.ProductsMarks.belongsTo(models.User)
+    //models.ProductsMarks.belongsTo(models.Product)
+    //models.Product.belongsToMany(models.User, { through: models.ProductsMarks })
+    //models.User.belongsToMany(models.Product, { through: models.ProductsMarks })
 }
 
 module.exports = ProductsMarks
