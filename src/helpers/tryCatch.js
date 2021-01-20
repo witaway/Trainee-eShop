011e79e19e02829f8cac88408ad925bd57e9031e
Yegor Levonenko <witaway33@gmail.com>
2021-01-20 03:08:26 +0300

    Added statuses to tryCatch helper

--- src/helpers/tryCatch.js
+++ src/helpers/tryCatch.js
@@ -4,7 +4,7 @@ module.exports = function tryCatch(func) {
     return async function(req, res) {
         const p = func(req, res) // func is async!
         p.catch((error) => {
-            res.status(404).json(error)
+            res.status(error.status || 520).json(error.message)
         })
     }
 }
