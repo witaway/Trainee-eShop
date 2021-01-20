1318d998e9cc70ddace83a2e55350f902d078860
Yegor Levonenko <witaway33@gmail.com>
2021-01-20 19:36:46 +0300

    #issue 6 - Hoooooray!!! A lot of work here, but a lot of new knowledges too.

--- src/repositories/product.js
+++ src/repositories/product.js
@@ -1,5 +1,8 @@
 const Product = require('../models/products/product')
-const { Op, Sequelize } = require("sequelize");
+const ProductsMarks = require('../models/products/productsMarks')
+
+const sequelize = require('../loaders/database')
+const { Op, Sequelize, DataTypes } = require("sequelize");
 
 class ProductRepository {
 
@@ -34,23 +37,38 @@ class ProductRepository {
         // }
         
         const settings = {
-            attributes: ['id', 'name', 'description', 'imageUrl', 'cost', 'quantity'],
-            order: [ [Sequelize.col('product.quantity'), Sequelize.literal(' = 0 ASC')] ]
+            attributes: ['id', 'name', 'description', 'imageUrl', 'cost', 'quantity', 
+            [sequelize.cast(sequelize.fn('AVG', sequelize.col('productsMarks.mark')), 'DECIMAL(12, 2)'), 'avg']], //cast for .00 precision
+            // Zeros at the bottom
+            order: [ [Sequelize.col('product.quantity'), Sequelize.literal(' = 0 ASC')] ],
+            include: {
+                model: ProductsMarks,
+                //Attribute avg moved to outer attributes because i don't need prefix tablename
+                //And if I ask some column in include it WILL add prefix tablename!
+                attributes: [],
+                where: {
+                    productId: sequelize.col('product.id')
+                },
+                required: false
+            },
+            group: [ sequelize.col('product.id') ],
+            // https://stackoverflow.com/questions/43729254/sequelize-limit-and-offset-incorrect-placement-in-query
+            // Without it LIMIT places incorrectly in subquery, but i need this just in the end.
+            subQuery: false,
+            raw: true
         }
         
-        if(options.sortBy && options.order && options.sortBy !== 'none') {
-            if(options.sortBy == 'date_of_update') options.sortBy = 'updatedAt'
-            settings.order.push(
-                [options.sortBy, options.order.toUpperCase()]
-            )
-        }
-
         if(options.onlyWithPictures === true) {
             settings.where = {
                 imageUrl: {
                     [Op.not]: null
                 }
             }
+        }
+
+        if(options.sortBy && options.order && options.sortBy !== 'none') {
+            if(options.sortBy === 'date_of_update') options.sortBy = 'updatedAt'
+            settings.order.push([options.sortBy, options.order])
         }
 
         if(options.count || options.count === 0) {
