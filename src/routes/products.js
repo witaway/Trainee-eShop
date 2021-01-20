43a7d568616b96601db13aa11f4a750b18486f5f
Yegor Levonenko <witaway33@gmail.com>
2021-01-20 23:36:32 +0300

    Now products api is rest-like

--- src/routes/products.js
+++ src/routes/products.js
@@ -9,12 +9,21 @@ const validator =  require('../helpers/validator')
 
 const ProductSchemas = require('../validationSchemas/products')
 
-router.post('/create',   validator(ProductSchemas.create),      tryCatch(ProductController.create))
-router.get('/get',       validator(ProductSchemas.getByID),     tryCatch(ProductController.getByID))
+// NOT REST
+//router.post('/create',   validator(ProductSchemas.create),      tryCatch(ProductController.create))
+//router.get('/get',       validator(ProductSchemas.getByID),     tryCatch(ProductController.getByID))
 //router.get('/list',      validator(ProductSchemas.getAll),      tryCatch(ProductController.getAll))
-router.get('/list',      validator(ProductSchemas.getWithList), tryCatch(ProductController.getWithList))
-router.put('/edit',      validator(ProductSchemas.edit),        tryCatch(ProductController.edit))
-router.delete('/delete', validator(ProductSchemas.delete),      tryCatch(ProductController.delete))
+//router.get('/list',      validator(ProductSchemas.getWithList), tryCatch(ProductController.getWithList))
+//router.put('/edit',      validator(ProductSchemas.edit),        tryCatch(ProductController.edit))
+//router.delete('/delete', validator(ProductSchemas.delete),      tryCatch(ProductController.delete))
+
+// REST
+router.get('/',          validator(ProductSchemas.getWithList),   tryCatch(ProductController.getWithList))
+router.get('/:id',       validator(ProductSchemas.getByID),       tryCatch(ProductController.getByID))
+router.delete('/:id',    validator(ProductSchemas.deleteByID),    tryCatch(ProductController.deleteByID))
+router.post('/',         validator(ProductSchemas.create),        tryCatch(ProductController.create))
+router.patch('/:id',     validator(ProductSchemas.editByID),      tryCatch(ProductController.editByID))
+
 
 router.use('/mark', MarkRouter)
 
