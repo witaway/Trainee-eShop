3ac04990ea3c0cbd43cdca60c3c10be3b5a5007e
Yegor Levonenko <witaway33@gmail.com>
2021-01-20 15:10:43 +0300

    Added default values to product scheme

--- src/validationSchemas/products.js
+++ src/validationSchemas/products.js
@@ -4,8 +4,8 @@ module.exports = {
 
     create: Joi.object({
         name: Joi.string().min(5).max(100).required(),
-        description: Joi.string().min(0).max(2000),
-        image: Joi.string().uri().max(300),
+        description: Joi.string().min(0).max(2000).default(''),
+        image: Joi.string().uri().max(300).default(null),
         cost: Joi.number().min(0).precision(2).required(),
         quantity: Joi.number().integer().min(0).required()
     }),
@@ -15,11 +15,11 @@ module.exports = {
     }),
 
     getWithList: Joi.object({
-        sort_by: Joi.string().valid('none', 'name', 'date_of_update', 'cost'),
+        sort_by: Joi.string().valid('none', 'name', 'date_of_update', 'cost').default('none'),
         order: Joi.string().valid('asc', 'desc').when('sort_by', {
             then: Joi.string().valid('asc', 'desc').required()
         }),
-        only_with_pictures: Joi.boolean(),
+        only_with_pictures: Joi.boolean().default(false),
         count: Joi.number().integer(),
         offset: Joi.number().integer()
     }),
@@ -29,14 +29,14 @@ module.exports = {
     edit: Joi.object({
         id: Joi.number().integer().required(),
         name: Joi.string().min(5).max(100).required(),
-        description: Joi.string().min(0).max(2000),
-        image: Joi.string().uri().max(300),
+        description: Joi.string().min(0).max(2000).default(''),
+        image: Joi.string().uri().max(300).default(null),
         cost: Joi.number().min(0).precision(2).required(),
         quantity: Joi.number().integer().min(0).required()
     }),
 
     delete: Joi.object({
-        id: Joi.number().integer()
+        id: Joi.number().integer().required()
     })
 
 }
