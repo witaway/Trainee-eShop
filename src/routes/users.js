cb323bd7fb72ccec5ce2cce86170d958ca5a0acb
Yegor Levonenko <witaway33@gmail.com>
2021-01-21 05:50:15 +0300

    Started writing users api

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,7 +5,11 @@ const UserController = require('../controllers/user')
 
 const tryCatch = require('../helpers/tryCatch');
 
-router.get('/list',  tryCatch(UserController.list));
-router.get('/get',   tryCatch(UserController.get));
+router.get('/',        tryCatch(UserController.getWithList));
+router.post('/',       tryCatch(UserController.create));
+
+router.get('/:id',     tryCatch(UserController.getByID));
+router.put('/:id',     tryCatch(UserController.updateByID));
+router.delete('/:id',  tryCatch(UserController.deleteByID));
 
 module.exports = router
