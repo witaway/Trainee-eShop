d5e395ba6eba6bc1d8b28bdd87ddde37009e9fa3
Yegor Levonenko <witaway33@gmail.com>
2021-01-21 05:50:43 +0300

    Not needed

--- src/routes/marks.js
+++ src/routes/marks.js
@@ -8,13 +8,12 @@ const validator =  require('../helpers/validator')
 
 const MarksSchemas = require('../validationSchemas/marks')
 
-
 router.get('/get',        validator(MarksSchemas.get),    tryCatch(MarkController.get));
 router.post('/set',       validator(MarksSchemas.set),    tryCatch(MarkController.set));
 router.delete('/delete',  validator(MarksSchemas.delete), tryCatch(MarkController.delete));
 
 //These will be deleted
-router.get('/getAverage', validator(MarksSchemas.getAverage), tryCatch(MarkController.getAverage));
-router.get('/getAll',     validator(MarksSchemas.getAll),     tryCatch(MarkController.getAll));
+//router.get('/getAverage', validator(MarksSchemas.getAverage), tryCatch(MarkController.getAverage));
+//router.get('/getAll',     validator(MarksSchemas.getAll),     tryCatch(MarkController.getAll));
 
 module.exports = router
