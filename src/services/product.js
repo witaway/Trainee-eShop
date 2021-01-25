4697976007259cc51bc799775a489f02ad71516c
Yegor Levonenko <witaway33@gmail.com>
2021-01-25 21:19:58 +0300

    Codestyle

--- src/services/product.js
+++ src/services/product.js
@@ -1,45 +1,55 @@
-const ProductRepository = require('../repositories/product')
-const { NotFoundException } = require('../classes/errors/4xx')
+const ProductRepository     = require('../repositories/product');
+const { NotFoundException } = require('../classes/errors/4xx');
 
 class ProductService {
 
     static async createProduct(productObject) {
-        const createdProduct = await ProductRepository.createProduct(productObject)
-        return createdProduct
+        const createdProduct = await ProductRepository.createProduct(productObject);
+        return createdProduct;
     }
 
     static async getProduct(id) {
-        const product = await ProductRepository.getProduct(id) 
-        if(!product) throw new NotFoundException("Product is not found")
-        return product
+        const product = await ProductRepository.getProduct(id);
+        if(!product) {
+            throw new NotFoundException("Product is not found");
+        }
+        return product;
     }
 
     static async getListOfProducts(options) {
-        const products = await ProductRepository.getListOfProducts(options)
-        if(!products) throw new NotFoundException("Product is not found")
-        return products
+        const products = await ProductRepository.getListOfProducts(options);
+        if(!products) {
+            throw new NotFoundException("Product is not found");
+        }
+        return products;
     }
 
     static async getAllProducts() {
-        const products = await ProductRepository.getAllProducts()
-        if(!products) throw new NotFoundException("Product is not found")
-        return products
+        const products = await ProductRepository.getAllProducts();
+        if(!products) {
+            throw new NotFoundException("Product is not found");
+        }
+        return products;
     }
 
     static async editProduct(id, productObject) {
-        const product = await ProductRepository.getProduct(id) 
-        if(!product) throw new NotFoundException("Product is not found")
-        const newProduct = await ProductRepository.editProduct(id, productObject)
-        return newProduct
+        const product = await ProductRepository.getProduct(id);
+        if(!product) {
+            throw new NotFoundException("Product is not found");
+        }
+        const newProduct = await ProductRepository.editProduct(id, productObject);
+        return newProduct;
     }
 
     static async deleteProduct(id) {
-        const product = await ProductRepository.getProduct(id) 
-        if(!product) throw new NotFoundException("Product is not found")
-        const deletedProduct = await ProductRepository.deleteProduct(id)
-        return deletedProduct
+        const product = await ProductRepository.getProduct(id);
+        if(!product) {
+            throw new NotFoundException("Product is not found");
+        }
+        const deletedProduct = await ProductRepository.deleteProduct(id);
+        return deletedProduct;
     }
 
 }
 
-module.exports = ProductService
+module.exports = ProductService;
