249a35c794a39b6f2dbc9c14649dc65a62c74b40
Yegor Levonenko <witaway33@gmail.com>
2021-01-25 04:11:55 +0300

    New message is a bit more common

--- src/constants/statusCodeMessages.js
+++ src/constants/statusCodeMessages.js
@@ -2,7 +2,7 @@ module.exports = {
     '400': 'The request contains a syntax error',
     '401': 'To access the requested resource requires authentication',
     '404': 'The resource is not found',
-    '409': 'The request could not be completed due to conflicting access to the resource',
+    '409': 'The request conflicts with the current state of the server',
     '410': 'The resource has been deleted and is no longer available',
     '422': 'Server cannot process the request because there is a logical error in a sent data',
     '449': 'Did not enough information',
