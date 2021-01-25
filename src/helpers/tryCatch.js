463302be0427fca72b108d4b9c17c72e3c61ea70
Yegor Levonenko <witaway33@gmail.com>
2021-01-25 03:25:41 +0300

    Now tryCatch makes uniform responses via uniform response creating module. Also it supports both expected and unexpected errors.

--- src/helpers/tryCatch.js
+++ src/helpers/tryCatch.js
@@ -1,12 +1,26 @@
 // tryCatch wrapper for controllers  
 module.exports = function tryCatch(func) {
 
+    const { ReasonPhrases, StatusCodes } = require('http-status-codes')
+    const statusCodeMessages = require('../constants/statusCodeMessages')
+
     return async function(req, res) {
         const p = func(req, res) // func is async!
-        p.catch((error) => {
-            res.status(error.status || 520).json({
-                message: error.message
-            })
+        p.catch((error) => {            
+            //If it's expected exception with defined http code
+            if(Object.keys(ReasonPhrases).includes(error.name)) {
+                // From my messages collection OR from standart reason messages list
+                const message = statusCodeMessages[error.status] || ReasonPhrases[StatusCodes[error.status]]
+                res.error(error.status, message, {
+                    name: error.name
+                })
+            //Or if it's totally unexpected and everything is BAD
+            } else {
+                res.error(500, 'Unexpected error', {
+                    name: error.name,
+                    stack: error.stack
+                })
+            }
         })
     }
 }
