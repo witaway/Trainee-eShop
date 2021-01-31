b50862bc6961e8ab8a9a2891bc8ccfb43f75eb7b
Yegor Levonenko <witaway33@gmail.com>
2021-02-01 00:51:43 +0300

    Added forbidden exception

--- src/classes/errors/4xx/index.js
+++ src/classes/errors/4xx/index.js
@@ -5,8 +5,10 @@ const NotFoundException              = require('./notFoundException');
 const RetryWithException             = require('./retryWithException');
 const UnauthorizedException          = require('./unauthorizedException');
 const UnprocessableEntityException   = require('./unprocessableEntityException');
+const ForbiddenException             = require('./forbiddenException');
 
 module.exports = {
+    ForbiddenException,
     BadRequestException,
     ConflictException,
     GoneException,
