65359971b6ed6b728f1a1e435d8b286c55b0b5db
Yegor Levonenko <witaway33@gmail.com>
2021-02-01 07:17:50 +0300

    Logging and checking of errors at startup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 const express = require('express');
+const { createInfo, createError } = require('./logger');
+
 const init = require('./loaders');
+const dbConnect = require('./logger').connect;
 
 (async () => {
-    const server = express();
-    await init(server);
+    
+    try {
+        
+        await dbConnect();
+        createInfo({
+            message: 'Logging database connected successfully'
+        });
+
+        const server = express();
+        await init(server);
+    } catch(err) {
+
+        console.log('Server is dead.');
+        console.log(err.name);
+        console.log(err.message);
+        console.log(err.stack);
+    
+        try {
+            createError({}, err);
+        } catch (ignoredError) { 
+            console.log('Logging is also dead:');
+            console.log(ignoredError);
+         }
+        
+    }
+
+
 })();
